6aedf3ef429875c092edb635499f1dc7cf0c1933
Albert <a19albchavas@inspedralbes.cat>
2022-10-20 12:04:01 +0200

    Day 1 completed
    Day 1 project is a very simple python program that asks the user two questions, in order to create a beer company, and prints the result of the concatenation of both answers.

---
+++ DAY1/main.py
@@ -0,0 +1,2 @@
+# Super basic program that ask the user two question for creating a beer company name
+print("Your beer company name is\n" + input("Favourite color? ") + " " + input("Favourite planet? "))
