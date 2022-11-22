51bfa9c4f2afbe51f5b0ba8499a9160e1e4a1742
Albert <albert.tardis05@gmail.com>
2022-11-22 02:10:21 +0100

    Refactor
    Changed completely the structure for implementing json and making the code more reliable. Also due to separate the code in different modules.

--- personal/voiceover_starcitizen/main.py
+++ personal/voiceover_starcitizen/main.py
@@ -1,36 +1,48 @@
 import keyboard as kb
-import speech_recognition as sr
-import pyttsx3
+from listening import speak, start_listening, stop_listening
 from tkinter import *
 from time import sleep
+import json_commands as jc
 
 
-WINDOW_TITLE: str = "StarCitizen - Voiceover"
+WINDOW_TITLE: str = "Star Citizen - Voiceover"
 WINDOW_RESOLUTION: str = "400x150+760+465"
+COMMAND_LIST: list = []
+FONTS: dict = {
+    "service-label": ("Dosis", 12, "bold"),
+    "info-frame": ("Dosis", 12, "bold"),
+    "info-title": ("Dosis", 20),
+    "title-command": ("Dosis", 18, "underline"),
+    "info-command": ("Dosis", 18),
+}
 
 active: bool = False
 button: Button
-info_text: Text
+info_text: Label
 app = Tk()
-audio = None
-recognizer = sr.Recognizer()
-mic = sr.Microphone()
-with mic as source:
-    recognizer.adjust_for_ambient_noise(source)
-recognizer.pause_threshold = 0.8
+config_window: Toplevel
 
 
 def main():
+    init_config()
     init_tkinter()
+    init_config_window()
 
+    service_frame = Frame(app, border=0)
+    service_frame.pack(side=TOP)
     # Informative text, service active (green) or stopped (red)
+    Label(
+        service_frame, font=FONTS["service-label"],
+        height=1, background="white",
+        text="Servicio:"
+    ).grid(row=0, column=0)
     global info_text
-    info_text = Text(
-        app, font=("Dosis", 12, "bold"), foreground="red", height=1
+    info_text = Label(
+        service_frame, font=FONTS["service-label"], foreground="red",
+        height=1, background="white"
     )
-    info_text.insert(END, "Servicio pausado")
-    info_text.config(state=DISABLED)
-    info_text.pack(side=TOP)
+    info_text.config(text="pausado")
+    info_text.grid(row=0, column=1)
 
     # Button for activating or stopping the listening service
     global button
@@ -39,6 +51,12 @@ def main():
         command=lambda: button_listen()
     )
     button.pack(side=BOTTOM)
+
+    # Information button for showing a list of possible voice commands
+    Button(
+        app, text="Información", width=15, height=1,
+        command=lambda: show_info_window()
+    ).pack()
 
     app.mainloop()
 
@@ -50,82 +68,103 @@ def init_tkinter():
     app.config(background="white")
 
 
-def listening_callback(recognize, audio_data):
+def init_config():
+    loaded_data = jc.load_json()
+    for command in loaded_data:
+        COMMAND_LIST.append(command)
+    print("Init config complete!")
+    print(COMMAND_LIST)
+
+
+def init_config_window():
+    global config_window
+    config_window = Toplevel(app)
+    config_window.title("Configuración de comandos")
+    config_window.geometry("1280x720")
+    Label(config_window, text="Configuración", font=FONTS["info-title"]).pack(side=TOP)
+
+    frame = Frame(config_window)
+    frame.pack(side=LEFT, expand=True, fill=BOTH)
+    for i in range(5):
+        option = ""
+        match i:
+            case 0:
+                option = "Comando"
+            case 1:
+                option = "Información"
+            case 2:
+                option = "Tecla"
+            case 3:
+                option = "Modificador"
+            case 4:
+                option = "Modo"
+        Label(
+            frame,
+            text=option,
+            font=FONTS["info-command"],
+        ).grid(
+            row=0,
+            column=i,
+            sticky="w"
+        )
+    for i, command_row in enumerate(COMMAND_LIST):
+        for j, data in enumerate(command_row):
+            Label(
+                frame,
+                text=command_row[data],
+                font=FONTS["title-command"] if j == 0 else FONTS["info-command"],
+                wraplength=800
+            ).grid(
+                row=i + 1,
+                column=j,
+                sticky="w" if j < 2 else "n"
+            )
+    config_window.withdraw()
+
+
+def show_info_window():
+    global config_window
     try:
-        listened = recognize.recognize_google(audio_data, language="es-ES")
-    except sr.UnknownValueError:
-        pass
-    except sr.RequestError as e:
-        speak("No se ha podido conectar con el servicio de reconocimiento de voz de Google."
-              "Intentalo más tarde")
-    else:
-        commands(listened)
+        if config_window.state() == "withdrawn":
+            config_window.deiconify()
+        elif config_window.state() == "normal":
+            config_window.withdraw()
+    except:
+        init_config_window()
+        config_window.deiconify()
 
 
 def button_listen():
     global active
     active = not active
-    info_text.config(state=NORMAL)
-    info_text.delete(1.0, END)
     if active:
-        global audio
         button.config(text="Parar")
-        info_text.config(foreground="green")
-        info_text.insert(END, "Servicio activo")
-        audio = recognizer.listen_in_background(source, listening_callback)
+        info_text.config(foreground="green", text="activo")
+        app.iconify()
+        speak("Voz activada")
+        start_listening()
     else:
         button.config(text="Escuchar")
-        info_text.config(foreground="red")
-        info_text.insert(END, "Servicio pausado")
+        info_text.config(foreground="red", text="pausado")
         stop_listening()
-    info_text.config(state=DISABLED)
 
 
 def commands(listened: str):
-    if "tren de aterrizaje" in listened:
-        kb.press_and_release("n")
-        speak("El tren de aterrizaje está en movimiento.")
-    elif "modo crucero" in listened:
-        kb.press_and_release("c")
-    elif "motor de salto" in listened:
-        kb.press_and_release("b")
-        speak("Iniciando sistemas, por favor espere.")
-    elif "salto cuántico" in listened:
-        kb.press("b")
-        sleep(1)
-        kb.release("b")
-        speak("Viajando al destino fijado.")
-    elif "energía" in listened:
-        kb.press_and_release("u")
-    elif "motores" in listened:
-        kb.press_and_release("i")
-    elif "para de escuchar" in listened:
-        button_listen()
-        speak("Vale, para volver a activarme dale al botón.")
-
-
-def speak(message: str) -> None:
-    """ Initiates the speech engine and says a message
-
-    :param message: sentence to say
-    """
-    engine = pyttsx3.init()
-    # TODO: do this for all pc
-    # voice_id = 0 or 1 = english
-    # voice_id = 2 = spanish
-    voice_id = engine.getProperty("voices")[2].id
-    engine.setProperty("voice", voice_id)
-    engine.say(message)
-    engine.runAndWait()
-
-
-def start_listening():
-    global audio
-    audio = recognizer.listen_in_background(source, listening_callback)
-
-
-def stop_listening():
-    audio(wait_for_stop=False)
+    for command in COMMAND_LIST:
+        if command["name"] in listened:
+            input_to_press = command["input"]
+            match command["modifier"]:
+                case 1:  # "control" modifier
+                    input_to_press = "ctrl+" + input_to_press
+                case 2:  # "alt" modifier
+                    input_to_press = "alt+" + input_to_press
+            match command["mode"]:
+                case 0:  # press
+                    kb.press_and_release(input_to_press)
+                case 1:  # hold
+                    kb.press(input_to_press)
+                    sleep(1)
+                    kb.release(input_to_press)
 
 
 if __name__ == '__main__':
